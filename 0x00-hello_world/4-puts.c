dde2fe802d6f0022eb2db358af8e66c0774f46af
John Macharia <machariajohn23@gmail.com>
2021-06-18 07:32:04 -0700

    Print in C using puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Prints string
- * Description - Prints "Programming is like building a multilingual puzzle"
+ * Description: Prints "Programming is like building a multilingual puzzle"
  * char - Contains the string to be printed
  *
  * return: "Programming is like building a multilingual puzzle". 0 otherwise
@@ -10,7 +10,8 @@
 int main(void)
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
-	puts (Mystr);
+
+	puts(Mystr);
 
 	return (0);
 
