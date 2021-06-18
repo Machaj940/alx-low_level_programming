cd6ac9ba91550653f8ac263b992984954632674c
John Macharia <machariajohn23@gmail.com>
2021-06-18 05:50:25 -0700

    Print a sentence using puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
-#include
+/**
+ * main - Entry point
+ * puts - Inserts sentence
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{puts("Programming is like building a multilingual puzzle);
+return(0);
+}
