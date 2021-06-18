12b74f21b387d1391d61da5dfde4db6523902805
John Macharia <machariajohn23@gmail.com>
2021-06-18 09:45:16 -0700

    Print a string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints string
+ * Description: Prints string and returns a value of 0 if false
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+
+	return (0);
+
+}
