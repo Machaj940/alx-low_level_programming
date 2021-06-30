7ddea5c6433e9bc9a7a3c75790a268febf990d76
John Macharia <machariajohn23@gmail.com>
2021-06-30 09:53:04 -0700

    Function to check for alphabetic characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _islower - function to check for lowercase characters
+ * Description: check for lowercase characters in a string
+ * @c: lowercase characters
+ * Return: Always 0(success)
+ */
+int _islower(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
