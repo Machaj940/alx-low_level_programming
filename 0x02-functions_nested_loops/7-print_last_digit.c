17ac216059d4190686eab18b12e83e9c4f8a8ebc
John Macharia <machariajohn23@gmail.com>
2021-06-30 10:49:17 -0700

    Function that prints the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @int: any number
+ *
+ * Return: Always 0
+ */
+int print_last_digit(int)
+{
+	int n;
+	int j = n % 10;
+
+	if (n > 0 || n < 0 || n == 0)
+	{
+		putchar(j);
+	}
+}
