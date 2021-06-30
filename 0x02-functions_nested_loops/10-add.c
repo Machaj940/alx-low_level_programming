fc4f7555e5afc429bde6a2f6e894605ba29aa5ac
John Macharia <machariajohn23@gmail.com>
2021-06-30 11:29:04 -0700

    Function that adds numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _add - adds any numbers
+ * @n: any number
+ * @j: any number
+ *
+ * Return: Always 0
+ */
+int add(int, int)
+{
+	int n;
+	int j;
+
+	return j+n;
+}
