dc0b494463fbc5681adebc6c3a02d2c0f9b32e96
John Macharia <machariajohn23@gmail.com>
2021-07-01 05:19:09 -0700

    Check for alphabetic character in a string

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- * _islower - function to check for lowercase characters
- * Description: check for lowercase characters in a string
- * @c: lowercase characters
+ * _isalpha - function to check for alphabet characters
+ * Description: check for alphabet characters in a string
+ * @c: alphabet character
  * Return: Always 0(success)
  */
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
