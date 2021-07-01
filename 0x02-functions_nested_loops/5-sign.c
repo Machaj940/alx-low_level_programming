2e5638fc1b8a7086e91dae9eb620b343b19b2161
John Macharia <machariajohn23@gmail.com>
2021-07-01 09:08:07 -0700

    Function that peints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,33 +2,23 @@
 /**
  * print_sign - prints the sign of a number
  * @n: any integer greater than or less than or 0
- *
  * Return: Always 0
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('43');
-		_putchar('44);
-		_putchar(' ');
+		_putchar(43);
 		return (1);
-		_putchar('\n');
 	}
 	else if (n == 0)
 	{
-		_putchar('45');
-		_putchar('44');
-		_putchar(' ');
-		return (-1);
-		_putchar('\n);
+		_putchar(48);
+		return (0);
 	}
 	else
 	{
-		_putchar('48');
-		_putchar('44');
-		_putchar(' ');
+		_putchar(45);
 		return (0);
-		_putchar('\n');
 	}
 }
