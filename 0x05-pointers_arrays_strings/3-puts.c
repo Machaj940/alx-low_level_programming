08076165a72701a247e19e0dc63f11edf6863bb5
John Macharia <machariajohn23@gmail.com>
2021-07-05 09:32:02 -0700

    Print a string and a newline

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,15 +2,13 @@
 /**
  * _puts - prints a string followed by a newline
  * @str: a string
- * @i: size of array
  * Return: str
  */
 void _puts(char *str)
 {
-	char *str[];
 	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 		_putchar('\n');
