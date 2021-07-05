19da42653b65e60690ea49e0a6f9a708baf0bf77
John Macharia <machariajohn23@gmail.com>
2021-07-05 08:20:22 -0700

    Print a string followed by a newline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _puts - prints a string followed by a newline
+ * @str: a string
+ * Return: str
+ */
+int _puts(char *str)
+{
+	char str[];
+
+	_putchar(str[]);
+	_putchar('\n');
+}
