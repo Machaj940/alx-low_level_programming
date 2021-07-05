013252cb5067880955e9fd4adfc468010fdd51e5
John Macharia <machariajohn23@gmail.com>
2021-07-05 12:17:40 -0700

    Print every other char of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * puts2 - prints every other char in a string, starting with the 1st char
+ * @str: a string
+ * _strlen: return the length of a string
+ * Return: str
+ */
+void puts2(char *str)
+{
+	int i;
+	int len;
+
+	len = _strlen(str);
+
+	for (i = 0; i < _strlen(str); i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - return the length of a string
+ * @s: a string
+ * Return: c
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
