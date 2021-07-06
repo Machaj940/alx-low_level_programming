7f6d5e689d2863cf22fa501424f256f87a2ee6b4
John Macharia <machariajohn23@gmail.com>
2021-07-06 10:33:06 -0700

    Copy n chars of a source string to a destination string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,7 +8,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *temp = dest;
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+
+	char *ptr = dest;
 
 	while (*src && n--)
 	{
@@ -17,5 +22,5 @@ char *_strncpy(char *dest, char *src, int n)
 		src++;
 	}
 	*dest = '\0';
-	return (temp);
+	return (ptr);
 }
