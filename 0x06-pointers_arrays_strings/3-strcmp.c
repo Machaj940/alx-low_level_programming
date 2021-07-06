6c3f7e98fb5df054548a5fd62340856c19db3b0e
John Macharia <machariajohn23@gmail.com>
2021-07-06 11:41:45 -0700

    Compare 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,11 +5,11 @@
  * @s2: string 2
  * Return: temp
  */
-char _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 == *s2)
 	{
-		if (*s1 == '\0')
+		if (*s1 == *s2)
 			return (0);
 	}
 
