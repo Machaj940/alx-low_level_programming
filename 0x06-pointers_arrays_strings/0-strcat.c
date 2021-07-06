81485a8a9bd65f72c97318ee0529f31f8d3c6f2d
John Macharia <machariajohn23@gmail.com>
2021-07-06 07:11:00 -0700

    Concatenate a string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,22 @@
 #include "holberton.h"
 /**
  * _strcat - concatenates two strings
- * @dest: a string
- * @src: a string
+ * @dest: string 1
+ * @src: string 2
  * Return: str3
  */
 char *_strcat(char *dest, char *src)
 {
-	char *str3[];
 	int i = 0, j = 0;
 
 	while (dest[i] != '\0')
-	{
-		str3[j] = dest[i];
 		i++;
-		j++;
-	}
 	while (src[j] != '\0')
 	{
-		str3[j] = src[j];
-		i++;
+		dest[i] = src[j];
 		j++;
+		i++;
 	}
-	str3[j] = '\0';
-	_putchar(str3);
+	dest[i] = '\0';
+        return(dest);
 }
