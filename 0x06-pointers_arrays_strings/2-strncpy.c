73092c1f870465900e6358c8c9240f8a7054e700
John Macharia <machariajohn23@gmail.com>
2021-07-09 10:48:27 -0700

    Copy a string using strncpy equivalent

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,8 +8,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
 
-	while (n-- && (*dest++ = *src++));
-	*dest = '\0';
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
