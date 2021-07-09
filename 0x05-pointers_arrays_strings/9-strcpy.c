27c4a9e6fb65f6a401a8da8318ce8d119a44327d
John Macharia <machariajohn23@gmail.com>
2021-07-09 08:28:25 -0700

    Copy source string into destination string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	char start = dest;
 
-	while (src[i] != '\0')
+	while (*src != '\0')
 	{
-		dest[i] = src[i];
-		i++;
+		*dest = *src;
+		dest++;
+		src++;
 	}
-	return (dest);
+	*dest = '\0';
+	return (start);
 }
