b39d4a92f90ef71583f68024d8ece1c4bf305e92
John Macharia <machariajohn23@gmail.com>
2021-07-09 07:56:21 -0700

    Copy source string into destination string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copy a string from source string to destination string
+ * @dest: destination string
+ * @src: source string
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
