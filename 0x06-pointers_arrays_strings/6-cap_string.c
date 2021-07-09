494c49c1b58a79fc07dcafc00aead41466b7fd1c
John Macharia <machariajohn23@gmail.com>
2021-07-09 15:32:38 -0700

    Capitalize all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalize all words of a string
+ * @s: string of chars
+ * Return: s
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if ((s[i] >= 'a' && s[i] <= 'z'))
+			{
+				s[i] = s[i] - 32;
+				continue;
+			}
+		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] = str[i] + 32;
+		}
+	}
+	return (s);
+}
