7c4981691ec4a565865742fcf48d99d12ab5d1e4
John Macharia <machariajohn23@gmail.com>
2021-07-14 04:05:40 -0700

    Function that fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,7 +9,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	unsigned char *memory = s, value = c;
+	unsigned char *memory = s, value = b;
 
 	for (i = 0; i < n; i++)
 		momory[i] = value;
