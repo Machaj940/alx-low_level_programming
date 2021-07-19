82d891e4016789fd75e5acd23e1bb449310e5bcb
John Macharia <machariajohn23@gmail.com>
2021-07-19 00:40:41 -0700

    Program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - program that prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This file contais code for argc-argv
