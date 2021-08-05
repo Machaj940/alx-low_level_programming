e01a36f8d1d19481ba47d92c0b0f380ed396af1a
John Macharia <machariajohn23@gmail.com>
2021-08-05 13:22:50 -0700

    Print the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+void print_b(unsigned long int n);
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to interpret as binary
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		_putchar('0');
+	else
+		print_b(n);
+}
+
+/**
+ * print_b - print the binary rep of a number
+ * @n: number to interpret as binary
+ */
+void print_b(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	print_b((n >> 1));
+	if ((n & 1) == 1)
+		_putchar ('1');
+	if ((n & 1) == 0)
+		_putchar('0');
+}
