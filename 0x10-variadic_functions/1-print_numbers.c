c86162e0687c997252d40bdd589f10e278479b1e
John Macharia <machariajohn23@gmail.com>
2022-01-06 09:43:11 -0800

    Print all the variable/additional arguments of a variadic function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers, followed by a new line
+ * @separator: string to be printed between numbers
+ * @n: the number of integers passed to the function
+ * Return: all args separated by separator
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, m;
+
+	if (separator == NULL)
+		return;
+
+	va_start(ap, n);         /* Initialize the argument list. */
+
+	for (i = 0; i < n; i++)
+	{
+		m = va_arg(ap, unsigned int);
+		printf("%d", m);
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+
+	va_end(ap);        /* Clean up. */
+	putchar('\n');
+}
