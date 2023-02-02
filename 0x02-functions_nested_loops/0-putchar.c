23617e4e99f5970eb15b3f729133e0e8243d738f
John Macharia <machariajohn23@gmail.com>
2023-02-02 22:32:04 +0300

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * main - Prints Holberton
+ * Description: Prints 'Holberton' using _putchar command
+ * Return: Always 0 (success)
+ */
+
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+        _putchar('\n');
+
+        return (0);
+}
